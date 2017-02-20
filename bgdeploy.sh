c0619edb4997240725b8e0ce1bd7c5b4c085e437
amitkumard <amitkumar@Amits-MacBook-Pro.local>
2017-02-21 00:12:56 +0530

    refined

--- bgdeploy.sh
+++ bgdeploy.sh
@@ -6,8 +6,7 @@ appStatusCheck(){
   GUID=$(cf app ${1} --guid)
   SUMMARY=$(cf curl "v2/apps/${GUID}/summary")
    NUM_INSTANCES=$(echo ${SUMMARY} | python -c 'import sys, json; print json.load(sys.stdin)["instances"]')
-  echo $NUM_INSTANCES
- n=0
+   n=0
   while [ ${RETURN} = "1" ] && [ ${n} -lt 10 ] ; do
     n=$(expr ${n} + 1)
     echo "Trying : ${n}/10 "
